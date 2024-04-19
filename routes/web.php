a3839b3b99718b18e9d50349a40ffbfdc4f2fcc1
carlos loitti <carlosloitir@gmail.com>
2024-04-19 15:50:49 -0500

    Vehiculo List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\VehiculoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    
 });
+
+Route::get('/vehiculos' , [VehiculoController::class, 'index'])->name('vehiculos.index');
