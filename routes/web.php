bc75cbb2654c885201f4bdc888db22ecbf62108d
carlos loitti <carlosloitir@gmail.com>
2024-04-20 18:56:05 -0500

    Breeze + Login + Register

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
-use App\Http\Controllers\PropietarioController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\VehiculoController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PropietarioController;
 
 Route::get('/', function () {
     return view('welcome');
-
-    
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 
 Route::get('/vehiculos' , [VehiculoController::class, 'index'])->name('vehiculos.index');
 Route::post('/vehiculos' , [VehiculoController::class, 'store'])->name('vehiculos.store');
@@ -35,3 +45,4 @@ Route::put('/servicios/{servicio}' , [ServicioController::class, 'update'])->nam
 Route::get('/servicios/{servicio}/edit' , [ServicioController::class, 'edit'])->name('servicios.edit');
 
 
+require __DIR__.'/auth.php';
