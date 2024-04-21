e6973faefb3e583b02e0d1aa77a718340d7c050d
Juan Londoño <juandavid1237890@gmail.com>
2024-04-20 19:31:01 -0500

    Vehiculos Middleware

--- routes/web.php
+++ routes/web.php
@@ -29,26 +29,29 @@ Route::middleware('auth')->group(function () {
     Route::get('/propietarios/{propietario}/edit' , [PropietarioController::class, 'edit'])->name('propietarios.edit');
 
 
+    Route::get('/servicios' , [ServicioController::class, 'index'])->name('servicios.index');
+    Route::post('/servicios' , [ServicioController::class, 'store'])->name('servicios.store');
+    Route::get('/servicios/create' , [ServicioController::class, 'create'])->name('servicios.create');
+    Route::delete('/servicios/{servicio}' , [ServicioController::class, 'destroy'])->name('servicios.destroy');
+    Route::put('/servicios/{servicio}' , [ServicioController::class, 'update'])->name('servicios.update');
+    Route::get('/servicios/{servicio}/edit' , [ServicioController::class, 'edit'])->name('servicios.edit');
+
+    Route::get('/vehiculos' , [VehiculoController::class, 'index'])->name('vehiculos.index');
+    Route::post('/vehiculos' , [VehiculoController::class, 'store'])->name('vehiculos.store');
+    Route::get('/vehiculos/create' , [VehiculoController::class, 'create'])->name('vehiculos.create');
+    Route::delete('/vehiculos/{vehiculo}' , [VehiculoController::class, 'destroy'])->name('vehiculos.destroy');
+    Route::put('/vehiculos/{vehiculo}' , [VehiculoController::class, 'update'])->name('vehiculos.update');
+    Route::get('/vehiculos/{vehiculo}/edit' , [VehiculoController::class, 'edit'])->name('vehiculos.edit');
 
 });
 
 
-Route::get('/vehiculos' , [VehiculoController::class, 'index'])->name('vehiculos.index');
-Route::post('/vehiculos' , [VehiculoController::class, 'store'])->name('vehiculos.store');
-Route::get('/vehiculos/create' , [VehiculoController::class, 'create'])->name('vehiculos.create');
-Route::delete('/vehiculos/{vehiculo}' , [VehiculoController::class, 'destroy'])->name('vehiculos.destroy');
-Route::put('/vehiculos/{vehiculo}' , [VehiculoController::class, 'update'])->name('vehiculos.update');
-Route::get('/vehiculos/{vehiculo}/edit' , [VehiculoController::class, 'edit'])->name('vehiculos.edit');
 
 
 
 
-Route::get('/servicios' , [ServicioController::class, 'index'])->name('servicios.index');
-Route::post('/servicios' , [ServicioController::class, 'store'])->name('servicios.store');
-Route::get('/servicios/create' , [ServicioController::class, 'create'])->name('servicios.create');
-Route::delete('/servicios/{servicio}' , [ServicioController::class, 'destroy'])->name('servicios.destroy');
-Route::put('/servicios/{servicio}' , [ServicioController::class, 'update'])->name('servicios.update');
-Route::get('/servicios/{servicio}/edit' , [ServicioController::class, 'edit'])->name('servicios.edit');
+
+
 
 
 require __DIR__.'/auth.php';
